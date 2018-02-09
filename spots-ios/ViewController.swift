28d6096af1eb1b357a6493bd69b8503cfc1bceec
Robin Gabre <robin.gabre@slagkryssaren.com>
2018-02-09 20:54:23 +0100

    Initial Commit

---
+++ spots-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spots-ios
+//
+//  Created by Robin Gabre on 2018-02-09.
+//  Copyright © 2018 Robin Gabre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ spots-iosTests/spots_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  spots_iosTests.swift
+//  spots-iosTests
+//
+//  Created by Robin Gabre on 2018-02-09.
+//  Copyright © 2018 Robin Gabre. All rights reserved.
+//
+
+import XCTest
+@testable import spots_ios
+
+class spots_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
