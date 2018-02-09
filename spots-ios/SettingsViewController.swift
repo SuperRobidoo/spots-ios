d18790928a7fc9b9b7b0d22a744ce9c48044b656
Robin Gabre <robin.gabre@slagkryssaren.com>
2018-02-09 22:02:31 +0100

    inital commit

---
+++ spots-ios/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  spots-ios
+//
+//  Created by Robin Gabre on 2018-02-09.
+//  Copyright © 2018 Robin Gabre. All rights reserved.
+//
+
+import Foundation
+
+class SettingsViewController: UIViewController {
+    
+    
+}
--- spots-ios/ViewController.swift
+++ spots-ios/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var addSpotButton: UIButton!
+    @IBOutlet weak var centerMapButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 37.36, longitude: -122.0, zoom: 6.0)
+        mapView.camera = camera
+        showMarker(position: camera.target)
+    }
+    
+    func showMarker(position: CLLocationCoordinate2D) {
+        let marker = GMSMarker()
+        marker.position = position
+        marker.title = "Palo Alto"
+        marker.snippet = "San Francisco"
+        marker.map = mapView
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addSpotButtonAction(_ sender: Any) {
+        print("addSpotButtonAction")
     }
 
+    @IBAction func centerMapButtonAction(_ sender: Any) {
+        print("centerMapButtonAction")
+    }
+    
+    @IBAction func settingsButtonAction(_ sender: Any) {
+        print("settingsButtonAction")
+    }
+    
+}
 
+extension ViewController: GMSMapViewDelegate {
+    /* handles Info Window tap */
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        print("didTapInfoWindowOf")
+    }
+    
+    /* handles Info Window long press */
+    func mapView(_ mapView: GMSMapView, didLongPressInfoWindowOf marker: GMSMarker) {
+        print("didLongPressInfoWindowOf")
+    }
+    
+    /* set a custom Info Window */
+    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        let view = UIView(frame: CGRect.init(x: 0, y: 0, width: 200, height: 70))
+        view.backgroundColor = UIColor.white
+        view.layer.cornerRadius = 6
+        
+        let lbl1 = UILabel(frame: CGRect.init(x: 8, y: 8, width: view.frame.size.width - 16, height: 15))
+        lbl1.text = "Hi there!"
+        view.addSubview(lbl1)
+        
+        let lbl2 = UILabel(frame: CGRect.init(x: lbl1.frame.origin.x, y: lbl1.frame.origin.y + lbl1.frame.size.height + 3, width: view.frame.size.width - 16, height: 15))
+        lbl2.text = "I am a custom info window."
+        lbl2.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        view.addSubview(lbl2)
+        
+        return view
+    }
 }
 
